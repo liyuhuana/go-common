a14d8c0ad4946f54993c68f4c80aeb92a5912964
李宇华 <liyh@163.com>
2020-11-25 18:23:05 +0700

    response size

--- framework/network/network_tcp/session.go
+++ framework/network/network_tcp/session.go
@@ -269,7 +269,7 @@ func (this *Session) Write(data []byte) error {
 func (this *Session) response(msgId, result int32, msgData []byte) error {
 	buf := new(bytes.Buffer)
 	int32Size := definition.UInt32ByteLen.Int()
-	err := binary.Write(buf, binary.LittleEndian, uint32(1+int32Size+int32Size+len(msgData)))
+	err := binary.Write(buf, binary.LittleEndian, uint32(int32Size+int32Size+len(msgData)))
 	if err != nil {
 		return err
 	}

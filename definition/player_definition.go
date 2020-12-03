bfa431b6c4af8edce396426db159dbb791d2bab5
李宇华 <liyh@163.com>
2020-12-03 14:32:16 +0800

    convert

--- definition/player_definition.go
+++ definition/player_definition.go
@@ -1,12 +1,23 @@
 package definition
 
-import "github.com/liyuhuana/go-common/utils/math_util"
+import (
+	"github.com/liyuhuana/go-common/utils/math_util"
+	"strconv"
+)
 
 type PlayerId int64
 
 const (
 	EmptyPlayerId PlayerId = -1
 )
+
+func ConvertStringToPlayerId(str string) PlayerId {
+	id, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return EmptyPlayerId
+	}
+	return PlayerId(id)
+}
 
 func (id PlayerId) Int64() int64 {
 	return int64(id)
